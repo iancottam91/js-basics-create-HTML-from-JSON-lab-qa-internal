77e7d88ef45b7a2e71bee8f48cd8dfba4407904b
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-19 19:17:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const title = movies.Titanic['title'];
-const director = movies['Titanic'].director;
-const genre = movies['Titanic'].genre;
 
-document.getElementById('title').innerText = title;
-document.getElementById('director').innerText = director;
-document.getElementById('genre').innerText = genre;
-
-
-
+function populateTitanic() {
+  const title = movies.Titanic['title'];
+  const director = movies['Titanic'].director;
+  const genre = movies['Titanic'].genre;
+  
+  document.getElementById('title').innerText = title;
+  document.getElementById('director').innerText = director;
+  document.getElementById('genre').innerText = genre;
+}
 
 
 
